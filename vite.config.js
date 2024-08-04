425a5168c8571368d4a069b987ed9aa835cc4fed
taunhealy <taunhealy@gmail.com>
2024-08-04 22:46:19 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
 
+// vite.config.js
 export default defineConfig({
   plugins: [eslintPlugin({ cache: false })],
   server: {
     host: 'localhost',
-    cors: false, // Disable CORS
+    cors: '*',
     hmr: {
       host: 'localhost',
       protocol: 'ws',
     },
-  },
-  optimizeDeps: {
-    include: ['gsap'],
   },
   build: {
     minify: true,
@@ -20,15 +18,15 @@ export default defineConfig({
     rollupOptions: {
       input: './src/main.js',
       output: {
-        format: 'umd', // Universal Module Definition for compatibility
-        entryFileNames: '[name].js', // Use placeholders for better file naming
+        format: 'umd',
+        entryFileNames: 'main.js',
         esModule: false,
         compact: true,
         globals: {
           jquery: '$',
         },
       },
-      external: ['jquery'], // jQuery will not be bundled, assumed to be loaded externally
+      external: ['jquery'],
     },
   },
 })
